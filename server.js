ddf4bfddcc093ec0a1a3b6aa658877b6c4b58734
CaueChieratto <caue.chieratto@icloud.com>
2026-02-10 11:20:16 -0300

    feat: instala e configura CORS no servidor

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const routes = require("./src/routes");
 const errorHandler = require("./src/middlewares/errorHandler");
 
 app.use(express.json());
+app.use(cors());
 
 const connectDatabase = require("./src/config/databaseConfig");
-connectDatabase();
 
 app.use(routes);
 app.use(errorHandler);
 
-if (require.main === module) {
-  app.listen(3000, () => {
-    console.log("🚀 Servidor rodando em http://localhost:3000");
-  });
-}
+connectDatabase().then(() => {
+  if (require.main === module) {
+    app.listen(3000, () => {
+      console.log("🚀 Servidor rodando em http://localhost:3000");
+    });
+  }
+});
 
 module.exports = app;
